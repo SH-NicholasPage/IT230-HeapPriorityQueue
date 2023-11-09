250470ebf37eed1aa4c9d69e5ef44fe585bc8222
Nicholas Page <npage@southhills.edu>
2023-11-08 22:27:27 -0500

    Initial commit

---
+++ HeapPQ/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Name: [YOUR NAME HERE]
+ * South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+ */
+
+namespace HeapPQ
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
